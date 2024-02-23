e4bb17f80e7d8fc02548d0dacd03b60ebb3550f9
Sujith <89125023+sujith-rek@users.noreply.github.com>
2024-02-24 03:04:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from TaskWindow import TaskWindow
 
 app = QApplication(sys.argv)
 
-# selected_folder = QFileDialog.getExistingDirectory()
-selected_folder = "C:\\Users\\somas\\OneDrive\\Documents\\tasks"
+selected_folder = QFileDialog.getExistingDirectory()
+# selected_folder = "C:\\Users\\somas\\OneDrive\\Documents\\tasks"
 
 window = TaskWindow(selected_folder)
 app.aboutToQuit.connect(window.save_before_close)
