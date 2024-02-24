7a4990be29c9866efb2fef0fa696705f871b7af1
Sujith <ps335@snu.edu.in>
2024-02-25 03:14:27 +0530

    remember folder

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QFileDialog
+from PyQt6.QtCore import QSettings
 import sys
 from TaskWindow import TaskWindow
 
-
 app = QApplication(sys.argv)
 
-selected_folder = QFileDialog.getExistingDirectory()
-# selected_folder = "C:\\Users\\somas\\OneDrive\\Documents\\tasks"
+settings = QSettings("MyCompany", "MyApp")
+
+selected_folder = settings.value("selected_folder", defaultValue="")
+if not selected_folder:
+    selected_folder = QFileDialog.getExistingDirectory()
+    if selected_folder:
+        settings.setValue("selected_folder", selected_folder)
 
 window = TaskWindow(selected_folder)
 app.aboutToQuit.connect(window.save_before_close)
